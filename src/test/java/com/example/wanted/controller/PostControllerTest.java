3da880a0c417b5564478d311b21732f0d865ebfb
haden <80521474+thdefn@users.noreply.github.com>
2023-08-05 22:28:11 +0900

    test : [007-post-read] 게시물 조회 기능

--- src/test/java/com/example/wanted/controller/PostControllerTest.java
+++ src/test/java/com/example/wanted/controller/PostControllerTest.java
@@ -12,14 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -59,6 +61,7 @@ class PostControllerTest {
                         .title("류승완 신작 ‘밀수’, 개봉 11일 만에 300만 돌파")
                         .content("무더위기 기승을 부리는 올여름 시원한 바다가 배경인 영화 ‘밀수’가 300만 관객을 돌파했다.")
                         .writerId(1L)
+                        .createdAt("1분 전")
                         .isReadersPost(true)
                         .build());
         //when
@@ -75,12 +78,13 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.title").value(form.getTitle()))
                 .andExpect(jsonPath("$.content").value(form.getContent()))
                 .andExpect(jsonPath("$.isReadersPost").value(true))
+                .andExpect(jsonPath("$.createdAt").value("1분 전"))
                 .andExpect(jsonPath("$.nickName").value("익명"));
     }
 
     @Test
     @WithMockCustomUser
-    void 게시물_생성_실패_1자에서_150자의_제목() throws Exception {
+    void 게시물_생성_실패_1자에서_150자의_제목이어야_함() throws Exception {
         //given
         PostForm form = PostForm.builder()
                 .title("무더위기 기승을 부리는 올여름 시원한 바다가 배경인 영화 ‘밀수’가 300만 관객을 돌파했다.\n" +
@@ -108,7 +112,7 @@ class PostControllerTest {
 
     @Test
     @WithMockCustomUser
-    void 게시물_생성_실패_1자에서_1000자의_내용() throws Exception {
+    void 게시물_생성_실패_1자에서_1000자의_내용이어야_함() throws Exception {
         //given
         PostForm form = PostForm.builder()
                 .title("류승완 신작 ‘밀수’, 개봉 11일 만에 300만 돌파")
@@ -140,6 +144,7 @@ class PostControllerTest {
                         .content("무더위기 기승을 부리는 올여름 시원한 바다가 배경인 영화 ‘밀수’가 300만 관객을 돌파했다.")
                         .writerId(1L)
                         .isReadersPost(true)
+                        .createdAt("1분 전")
                         .build());
         //when
         //then
@@ -155,12 +160,13 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.title").value(form.getTitle()))
                 .andExpect(jsonPath("$.content").value(form.getContent()))
                 .andExpect(jsonPath("$.isReadersPost").value(true))
+                .andExpect(jsonPath("$.createdAt").value("1분 전"))
                 .andExpect(jsonPath("$.nickName").value("익명"));
     }
 
     @Test
     @WithMockCustomUser
-    void 게시물_수정_실패_1자에서_150자의_제목() throws Exception {
+    void 게시물_수정_실패_1자에서_150자의_제목이어야_함() throws Exception {
         //given
         PostForm form = PostForm.builder()
                 .title("무더위기 기승을 부리는 올여름 시원한 바다가 배경인 영화 ‘밀수’가 300만 관객을 돌파했다.\n" +
@@ -189,7 +195,7 @@ class PostControllerTest {
 
     @Test
     @WithMockCustomUser
-    void 게시물_수정_실패_1자에서_1000자의_내용() throws Exception {
+    void 게시물_수정_실패_1자에서_1000자의_내용이어야_함() throws Exception {
         //given
         PostForm form = PostForm.builder()
                 .title("류승완 신작 ‘밀수’, 개봉 11일 만에 300만 돌파")
@@ -204,6 +210,84 @@ class PostControllerTest {
                 )
                 .andDo(print())
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @WithMockCustomUser
+    void 게시물_리스트_조회_성공() throws Exception {
+        //given
+        Page<PostDto> dto = new PageImpl<>(
+                List.of(PostDto.builder()
+                                .postId(3L)
+                                .title("류승완 신작 ‘밀수’, 개봉 11일 만에 300만 돌파")
+                                .content("무더위기 기승을 부리는 올여름 시원한 바다가 배경인 영화 ‘밀수’가 300만 관객을 돌파했다.")
+                                .writerId(1L)
+                                .nickName("익명")
+                                .createdAt("2분 전")
+                                .build(),
+                        PostDto.builder()
+                                .postId(2L)
+                                .title("논란 이어지는 잼버리, 종교계가 구원 나섰다")
+                                .content("제대로 준비되지 않은 행사 진행으로 여러 논란을 낳고 있는 세계스카우트잼버리 대회를 위해 종교계가 나섰다.")
+                                .writerId(1L)
+                                .nickName("익명")
+                                .createdAt("1일 전")
+                                .build(),
+                        PostDto.builder()
+                                .postId(1L)
+                                .title("극초음속 미사일 장착 시작하는 美 줌월트급 구축함 [최현호의 무기인사이드]")
+                                .content("지난 1일(현지시각), 미 해군의 스텔스 구축함 USS 줌왈트가 극초음속 미사일 발사 장비 장착을 위해 샌디에이고를 출발해 미시시피주 파스카굴라로 출발했다.")
+                                .writerId(2L)
+                                .nickName("익명")
+                                .createdAt("1일 전")
+                                .build()));
+        given(postService.read(anyInt())).willReturn(dto);
+        //when
+        //then
+        mockMvc.perform(get("/posts")
+                        .param("page", "0")
+                        .header("Authorization", TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content.[0].postId").value(3L))
+                .andExpect(jsonPath("$.content.[0].writerId").value(1L))
+                .andExpect(jsonPath("$.content.[0].title").value("류승완 신작 ‘밀수’, 개봉 11일 만에 300만 돌파"))
+                .andExpect(jsonPath("$.content.[0].content").value("무더위기 기승을 부리는 올여름 시원한 바다가 배경인 영화 ‘밀수’가 300만 관객을 돌파했다."))
+                .andExpect(jsonPath("$.content.[0].nickName").value("익명"))
+                .andExpect(jsonPath("$.content.[0].createdAt").value("2분 전"));
+    }
+
+    @Test
+    @WithMockCustomUser
+    void 게시물_상세_조회_성공() throws Exception {
+        //given
+        given(postService.detail(anyLong(), any()))
+                .willReturn(PostDto.builder()
+                        .postId(1L)
+                        .title("류승완 신작 ‘밀수’, 개봉 11일 만에 300만 돌파")
+                        .content("무더위기 기승을 부리는 올여름 시원한 바다가 배경인 영화 ‘밀수’가 300만 관객을 돌파했다.")
+                        .writerId(1L)
+                        .nickName("익명")
+                        .isReadersPost(true)
+                        .createdAt("1분 전")
+                        .build());
+        //when
+        //then
+        mockMvc.perform(get("/posts/{postId}", 1L)
+                        .header("Authorization", TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.postId").value(1L))
+                .andExpect(jsonPath("$.writerId").value(1L))
+                .andExpect(jsonPath("$.title").value("류승완 신작 ‘밀수’, 개봉 11일 만에 300만 돌파"))
+                .andExpect(jsonPath("$.content").value("무더위기 기승을 부리는 올여름 시원한 바다가 배경인 영화 ‘밀수’가 300만 관객을 돌파했다."))
+                .andExpect(jsonPath("$.isReadersPost").value(true))
+                .andExpect(jsonPath("$.createdAt").value("1분 전"))
+                .andExpect(jsonPath("$.nickName").value("익명"));
     }
 
 }
--- src/test/java/com/example/wanted/service/PostServiceTest.java
+++ src/test/java/com/example/wanted/service/PostServiceTest.java
@@ -13,11 +13,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
+import static com.example.wanted.exception.ErrorCode.POST_NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -50,6 +56,7 @@ class PostServiceTest {
                         .title("류승완 신작 ‘밀수’, 개봉 11일 만에 300만 돌파")
                         .content("무더위기 기승을 부리는 올여름 시원한 바다가 배경인 영화 ‘밀수’가 300만 관객을 돌파했다.")
                         .writer(member)
+                        .createdAt(LocalDateTime.now())
                         .build());
         //when
         PostDto dto = postService.create(form, member);
@@ -58,6 +65,7 @@ class PostServiceTest {
         assertEquals(1L, dto.getPostId());
         assertEquals(form.getTitle(), dto.getTitle());
         assertEquals(form.getContent(), dto.getContent());
+        assertTrue(dto.getCreatedAt().contains("초 전"));
         assertEquals(1L, dto.getWriterId());
         assertTrue(dto.getIsReadersPost());
     }
@@ -75,6 +83,7 @@ class PostServiceTest {
                         .title("류승완 신작 ‘밀수’, 개봉 11일 만에 300만 돌파")
                         .content("무더위기 기승을 부리는 올여름 시원한 바다가 배경인 영화 ‘밀수’가 300만 관객을 돌파했다.")
                         .writer(member)
+                        .createdAt(LocalDateTime.now())
                         .build()));
         //when
         PostDto dto = postService.update(1L, form, member);
@@ -98,7 +107,7 @@ class PostServiceTest {
         PostException exception = assertThrows(PostException.class,
                 () -> postService.update(1L, form, member));
         //then
-        assertEquals(ErrorCode.POST_NOT_FOUND, exception.getErrorCode());
+        assertEquals(POST_NOT_FOUND, exception.getErrorCode());
     }
 
     @Test
@@ -123,6 +132,104 @@ class PostServiceTest {
                 () -> postService.update(1L, form, member));
         //then
         assertEquals(ErrorCode.MEMBER_NOT_WRITER, exception.getErrorCode());
+    }
+
+    @Test
+    void 게시물_리스트_조회_성공() {
+        //given
+        Member writer = Member.builder()
+                .id(2L)
+                .build();
+        Page<Post> pages = new PageImpl<>(
+                List.of(Post.builder()
+                                .id(3L)
+                                .title("류승완 신작 ‘밀수’, 개봉 11일 만에 300만 돌파")
+                                .content("금산사 계곡에서 더위를 날리고 있는 청소년들. 한국불교문화사업단 제공 한국불교문화사업단은 새만금 인근 사찰에서 청소년들의 쾌적한 전통문화 체험을 지원하고 있다. 김제 금산사는 당초 예정된 템금산사 계곡에서 더위를 날리고 있는 청소년들. 한국불교문화사업단 제공 한국불교문화사업단은 새만금 인근 사찰에서 청소년들의 쾌적한 전통문화 체험을 지원하고 있다. 김제 금산사는 당초 예정된 템금산사 계곡에서 더위를 날리고 있는")
+                                .createdAt(LocalDateTime.now())
+                                .writer(writer)
+                                .build(),
+                        Post.builder()
+                                .id(2L)
+                                .title("논란 이어지는 잼버리, 종교계가 구원 나섰다")
+                                .content("제대로 준비되지 않은 행사 진행으로 여러 논란을 낳고 있는 세계스카우트잼버리 대회를 위해 종교계가 나섰다.")
+                                .createdAt(LocalDateTime.now())
+                                .writer(writer)
+                                .build(),
+                        Post.builder()
+                                .id(1L)
+                                .title("극초음속 미사일 장착 시작하는 美 줌월트급 구축함 [최현호의 무기인사이드]")
+                                .content("지난 1일(현지시각), 미 해군의 스텔스 구축함 USS 줌왈트가 극초음속 미사일 발사 장비 장착을 위해 샌디에이고를 출발해 미시시피주 파스카굴라로 출발했다.")
+                                .createdAt(LocalDateTime.now())
+                                .writer(writer)
+                                .build()));
+        given(postRepository.findPostByOrderByIdDesc(any())).willReturn(pages);
+        //when
+        Page<PostDto> dtos = postService.read(0);
+        //then
+        assertEquals(3L, dtos.getContent().get(0).getPostId());
+        assertEquals(150, dtos.getContent().get(0).getContent().length());
+        assertTrue(dtos.getContent().get(0).getCreatedAt().contains("초 전"));
+        assertEquals(2L, dtos.getContent().get(0).getWriterId());
+    }
+
+    @Test
+    void 게시물_상세_조회_성공_조회자가_게시물_작성자임() {
+        //given
+        Member writer = Member.builder()
+                .id(2L)
+                .build();
+        given(postRepository.findById(anyLong()))
+                .willReturn(Optional.of(Post.builder()
+                        .id(3L)
+                        .title("류승완 신작 ‘밀수’, 개봉 11일 만에 300만 돌파")
+                        .content("금산사 계곡에서 더위를 날리고 있는 청소년들. 한국불교문화사업단 제공 한국불교문화사업단은 새만금 인근 사찰에서 청소년들의 쾌적한 전통문화 체험을 지원하고 있다. 김제 금산사는 당초 예정된 템금산사 계곡에서 더위를 날리고 있는 청소년들. 한국불교문화사업단 제공 한국불교문화사업단은 새만금 인근 사찰에서 청소년들의 쾌적한 전통문화 체험을 지원하고 있다. 김제 금산사는 당초 예정된 템금산사 계곡에서 더위를 날리고 있는")
+                        .createdAt(LocalDateTime.now())
+                        .writer(writer)
+                        .build()));
+        //when
+        PostDto dto = postService.detail(3L, writer);
+        //then
+        assertEquals(3L, dto.getPostId());
+        assertNotEquals(150, dto.getContent().length());
+        assertTrue(dto.getCreatedAt().contains("초 전"));
+        assertEquals(2L, dto.getWriterId());
+        assertTrue(dto.getIsReadersPost());
+    }
+
+    @Test
+    void 게시물_상세_조회_성공_조회자가_게시물_작성자가_아님() {
+        //given
+        Member writer = Member.builder()
+                .id(2L)
+                .build();
+        given(postRepository.findById(anyLong()))
+                .willReturn(Optional.of(Post.builder()
+                        .id(3L)
+                        .title("류승완 신작 ‘밀수’, 개봉 11일 만에 300만 돌파")
+                        .content("금산사 계곡에서 더위를 날리고 있는 청소년들. 한국불교문화사업단 제공 한국불교문화사업단은 새만금 인근 사찰에서 청소년들의 쾌적한 전통문화 체험을 지원하고 있다. 김제 금산사는 당초 예정된 템금산사 계곡에서 더위를 날리고 있는 청소년들. 한국불교문화사업단 제공 한국불교문화사업단은 새만금 인근 사찰에서 청소년들의 쾌적한 전통문화 체험을 지원하고 있다. 김제 금산사는 당초 예정된 템금산사 계곡에서 더위를 날리고 있는")
+                        .createdAt(LocalDateTime.now())
+                        .writer(writer)
+                        .build()));
+        //when
+        PostDto dto = postService.detail(3L, member);
+        //then
+        assertEquals(3L, dto.getPostId());
+        assertNotEquals(150, dto.getContent().length());
+        assertTrue(dto.getCreatedAt().contains("초 전"));
+        assertEquals(2L, dto.getWriterId());
+        assertFalse(dto.getIsReadersPost());
+    }
+
+    @Test
+    void 게시물_상세_조회_실패() {
+        //given
+        given(postRepository.findById(anyLong()))
+                .willReturn(Optional.empty());
+        //when
+        PostException exception = assertThrows(PostException.class,
+                () -> postService.detail(3L, member));
+        //then
+        assertEquals(POST_NOT_FOUND, exception.getErrorCode());
     }
 
 }
